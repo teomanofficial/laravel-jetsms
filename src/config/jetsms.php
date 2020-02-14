2d34b3ebe3559653b655de36da158a8ac34a5cf6
hsntngr <hasantngr.53qgmail.com>
2020-02-14 18:23:26 +0300

    endpoint değiştirildi, originator eklendi

--- src/config/jetsms.php
+++ src/config/jetsms.php
@@ -5,11 +5,12 @@ return [
     // isteğin gönderileceği adres
     // jet sms tarafından api üzerinde versiyon level
     // güncelleme olmadıkça bu adresi değiştirmeyiniz.
-    'endpoint' => 'www.biotekno.biz:8080/SMS-Web/xmlsms',
+    'endpoint' => 'https://api.jetsms.com.tr/SMS-Web/HttpSmsSend',
 
     // api kullanıcı bilgileri
     'auth' => [
         'username' => 'hsntngr',
-        'password' => 'secret'
+        'password' => 'secret',
+        'originator' => 'laravel'
     ]
 ];
